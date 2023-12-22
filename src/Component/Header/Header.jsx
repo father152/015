1a414070b98c639b22e697110c39352766f3acf0
Your Name <tsimbalenko.vv@gmail.com>
2023-12-22 15:45:06 +0200

    23

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -6,22 +6,95 @@ import { Link } from "react-router-dom";
 
 
 function Header() {
-  return (
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open)};
+  
+    return (
     <div className="header">
       <div className="header-container">
         <div className="logo">
           <Link to="/"><img src={picture} alt=""></img></Link>
         </div>
         <div className="list-header">
-        <ul>
-       <li><Link to='/about'>About As</Link></li>
-       <li><Link to='/history'>History</Link></li>
-       <li><Link to='/portfolio'>Portfolio</Link></li>
-       <li><Link to='/contact'>Contact</Link></li>
+        
+      
+
+      <div className="dropdown">
+        <button onClick={handleOpen}>ABOUT AS</button>
+        {open ? (
+          <ul className="menu">
+            <li className="menu-item">
+              Past
+            </li>
+            <li className="menu-item">
+              History
+            </li>
+            <li className="menu-item">
+              Future
+            </li>
+          </ul>
+        ) : null}
+          </div>
+          <div className="dropdown2">
+          <button onClick={handleOpen}>ABOUT AS</button>
+          {open ? (
+            <ul className="menu">
+              <li className="menu-item">
+                Past
+              </li>
+              <li className="menu-item">
+                History
+              </li>
+              <li className="menu-item">
+                Future
+              </li>
+            </ul>
+          ) : null}
+            </div>
+            <div className="dropdown3">
+            <button onClick={handleOpen}>ABOUT AS</button>
+            {open ? (
+              <ul className="menu">
+                <li className="menu-item">
+                  Past
+                </li>
+                <li className="menu-item">
+                  History
+                </li>
+                <li className="menu-item">
+                  Future
+                </li>
+              </ul>
+            ) : null}
+              </div>
+              <div className="dropdown4">
+              <button onClick={handleOpen}>ABOUT AS</button>
+              {open ? (
+                <ul className="menu">
+                  <li className="menu-item">
+                    Past
+                  </li>
+                  <li className="menu-item">
+                    History
+                  </li>
+                  <li className="menu-item">
+                    Future
+                  </li>
                 </ul>
+              ) : null}
+                </div>
+
+
+
+
+
+        
         </div>
       </div>
     </div>
   );
 }
-export { Header };
+
+export {Header}
