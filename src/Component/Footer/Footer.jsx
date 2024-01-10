f31e85e746d80eab0e6b3c14e1856473afd57a6b
Your Name <tsimbalenko.vv@gmail.com>
2024-01-10 16:48:18 +0200

    6

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 import "./footer.css";
-import telegram from "../images/free-icon-telegram-2111646.png"
-import facebook from "../images/free-icon-facebook-174848.png"
-import twitter from "../images/free-icon-twitter-4494477.png"
-
+import telegram from "../images/free-icon-telegram-2111646.png";
+import facebook from "../images/free-icon-facebook-174848.png";
+import twitter from "../images/free-icon-twitter-4494477.png";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <div className="footer">
       <h3>All Rigth Reserved &#169; 2023</h3>
       <div className="social">
-      <ul>
-      <li><img src= {twitter} alt=""></img></li>
-      <li><img src= {facebook} alt=""></img></li>
-      <li><img src= {telegram} alt=""></img></li>
-      </ul>
+        <ul>
+          <li>
+            <Link to="https://twitter.com" target="_blank">
+              <img src={twitter} alt=""></img>
+            </Link>
+          </li>
+          <li>
+            <Link to="https://www.facebook.com/?locale=uk_UA" target="_blank">
+              <img src={facebook} alt=""></img>
+            </Link>
+          </li>
+          <li>
+            <Link to="https://web.telegram.org/k/" target="_blank">
+              <img src={telegram} alt=""></img>
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
