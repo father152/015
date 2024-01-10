1f0605ee39c173485da88e70272bb4770b27ba51
Your Name <tsimbalenko.vv@gmail.com>
2024-01-10 16:05:35 +0200

    45

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./footer.css";
+import telegram from "../images/free-icon-telegram-2111646.png"
+import facebook from "../images/free-icon-facebook-174848.png"
+import twitter from "../images/free-icon-twitter-4494477.png"
 
 
 function Footer() {
@@ -8,9 +11,9 @@ function Footer() {
       <h3>All Rigth Reserved &#169; 2023</h3>
       <div className="social">
       <ul>
-      <li className="twitter"></li>
-      <li className="facebook"></li>
-      <li className="telegram"></li>
+      <li><img src= {twitter} alt=""></img></li>
+      <li><img src= {facebook} alt=""></img></li>
+      <li><img src= {telegram} alt=""></img></li>
       </ul>
       </div>
     </div>
